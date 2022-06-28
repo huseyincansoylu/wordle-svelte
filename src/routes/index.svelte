0ed124483ae3df7ba67356aaca8b420c5a93b707
huseyin <huseyin@keyko.io>
2022-06-28 21:58:22 +0300

    added close modal functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,6 +23,9 @@ function playAgain() {
     GAME_WORD.set(getRandomWord())
 }
 
+function closeModal () {
+    playAgain()
+}
 
 
 </script>
@@ -35,7 +38,7 @@ function playAgain() {
      
     {#if $gameOver}
        <div transition:fade class="bg-white z-10 rounded-lg absolute w-[520px] h-[420px] shadow-2xl translate-y-[20%] p-4 flex flex-col items-center justify-evenly">
-        <span class="absolute top-4 right-4 cursor-pointer">
+        <span class="absolute top-4 right-4 cursor-pointer" on:click={closeModal}>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
@@ -46,7 +49,8 @@ function playAgain() {
           <p class="text-center font-semibold mt-6">YOU NAILED IT!</p> 
       </div>
        {:else}
-        <div>THE WORD WAS {$GAME_WORD}</div>
+        <div class="text-2xl">THE WORD WAS</div>
+        <div class="text-2xl">"{$GAME_WORD}"</div>
     {/if}
         
        
