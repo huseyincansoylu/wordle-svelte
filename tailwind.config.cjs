6ee9c40137c4dd492bd4452cad81757f7dd5519c
huseyin <huseyin@keyko.io>
2022-06-28 22:51:31 +0300

    extend tailwind theme for modal shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.4)',
+      }
+    },
   },
   plugins: [],
 }
